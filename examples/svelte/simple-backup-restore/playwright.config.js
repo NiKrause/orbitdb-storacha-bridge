f9a38b759f582a0cea0ea2687673e1e5e8b90b3e
Nico Krause <post@nicokrause.com>
2025-12-03 20:40:45 +0500

    chore: remove old eslintrc and update playwright configs

--- examples/svelte/simple-backup-restore/playwright.config.js
+++ examples/svelte/simple-backup-restore/playwright.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from "@playwright/test";
 
+const port = process.env.PORT
+  ? parseInt(process.env.PORT, 10)
+  : 5173;
+
 export default defineConfig({
   webServer: {
     command: "npm run dev",
-    port: 5173,
+    port: port,
     reuseExistingServer: !process.env.CI,
   },
   testDir: "e2e",
   use: {
-    baseURL: "http://localhost:5173",
+    baseURL: `http://localhost:${port}`,
   },
 });
